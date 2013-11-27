e07345bd03c2de4027c3370b9eae7627dee7e866
Ben Letton <ben.letton@tbone-tv.com>
2013-11-27 21:35:55 +0000

    Roman Numerals, simple, ugly but works

--- src/test/scala/benoit/numerals/NumeralsTest.scala
+++ src/test/scala/benoit/numerals/NumeralsTest.scala
@@ -7,106 +7,108 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class NumeralsTest extends FunSuite {
 
-  val One    : String = "I"
-  val Five   : String = "V"
-  val Ten    : String = "X"
-  val Fifty  : String = "L"
-  val Hundred: String = "C"
 
-  def numeralFor(digit: Int): String = {
-    if(digit >= 100)     Hundred + numeralFor(digit - 100)
-    else if(digit >= 90) numeralFor(10) + Hundred + numeralFor(digit - 90)
-    else if(digit >= 50) Fifty + numeralFor(digit - 50)
-    else if(digit == 40) numeralFor(10) + Fifty
-    else if(digit >= 10) Ten + numeralFor(digit - 10)
-    else if(digit == 9)  ones(1) + Ten
-    else if(digit >= 5)  Five + ones(digit - 5)
-    else if(digit == 4)  ones(1) + Five
-    else if(digit > 0)   ones(digit)
-    else ""
+  def toNumeral(digit: Int): String = {
+    if(digit >= 500) "D" + toNumeral(digit - 500)
+    else if(digit >= 400) "CD" + toNumeral(digit - 400)
+    else if(digit >= 100) "C" + toNumeral(digit - 100)
+    else if(digit >= 90) "XC" + toNumeral(digit - 90)
+    else if(digit >= 50) "L" + toNumeral(digit - 50)
+    else if(digit >= 40) "XL" + toNumeral(digit - 40)
+    else if(digit >= 10) "X" + toNumeral(digit - 10)
+    else if(digit >= 9) "IX" + toNumeral(digit - 9)
+    else if(digit >= 5) "V" + toNumeral(digit - 5)
+    else if(digit >= 4) "IV" + toNumeral(digit - 4)
+    else (for(i <- 1 to digit by 1) yield "I") mkString
   }
 
-  def ones(digit: Int): String = {
-    (for(i <- 1 to digit by 1) yield { One }) mkString
+  test("501 should be D") {
+    assert(toNumeral(501) === "DI")
   }
 
-  test("range check 1..150") {
-
-    for(digit <- 1 to 150 by 1) {
-      println(numeralFor(digit))
-    }
+  test("500 should be D") {
+    assert(toNumeral(500) === "D")
   }
 
-  test("101 should be CI") {
-    assert( numeralFor(101) === "CI")
+  test("400 should be CC") {
+    assert(toNumeral(400) === "CD")
   }
 
-  test("98 should be XCVIII") {
-    assert( numeralFor(98) === "XCVIII")
+  test("200 should be CC") {
+    assert(toNumeral(200) === "CC")
   }
 
-  test("90 should be XC") {
-    assert( numeralFor(90) === "XC")
-  }
-
-  test("80 should be LXXX") {
-    assert( numeralFor(80) === "LXXX")
+  test("48 should be XLVIII") {
+    assert(toNumeral(48) === "XLVIII")
   }
 
   test("1 should be I") {
-    assert( numeralFor(1) === "I")
+    assert(toNumeral(1) === "I")
   }
 
   test("2 should be II") {
-    assert( numeralFor(2) === "II")
+    assert(toNumeral(2) === "II")
   }
 
   test("4 should be IV") {
-    assert( numeralFor(4) === "IV")
+    assert(toNumeral(4) === "IV")
   }
 
   test("5 should be V") {
-    assert( numeralFor(5) === "V")
+    assert(toNumeral(5) === "V")
   }
 
   test("6 should be VI") {
-    assert( numeralFor(6) === "VI")
+    assert(toNumeral(6) === "VI")
   }
 
   test("7 should be VII") {
-    assert( numeralFor(7) === "VII")
+    assert(toNumeral(7) === "VII")
   }
 
   test("9 should be IX") {
-    assert( numeralFor(9) === "IX")
+    assert(toNumeral(9) === "IX")
   }
 
   test("10 should be X") {
-    assert( numeralFor(10) === "X")
+    assert(toNumeral(10) === "X")
   }
 
   test("14 should be XIV") {
-    assert( numeralFor(14) === "XIV")
+    assert(toNumeral(14) === "XIV")
   }
 
   test("15 should be XV") {
-    assert( numeralFor(15) === "XV")
+    assert(toNumeral(15) === "XV")
   }
 
   test("30 should be XXX") {
-    assert( numeralFor(30) === "XXX")
+    assert(toNumeral(30) === "XXX")
   }
 
   test("40 should be XL") {
-    assert( numeralFor(40) === "XL")
+    assert(toNumeral(40) === "XL")
   }
 
   test("50 should be L") {
-    assert( numeralFor(50) === "L")
+    assert(toNumeral(50) === "L")
   }
 
   test("60 should be LX") {
-    assert( numeralFor(60) === "LX")
+    assert(toNumeral(60) === "LX")
   }
+
+  test("98 should be XCVIII") {
+    assert(toNumeral(98) === "XCVIII")
+  }
+
+  test("90 should be XC") {
+    assert(toNumeral(90) === "XC")
+  }
+
+  test("80 should be LXXX") {
+    assert(toNumeral(80) === "LXXX")
+  }
+
 
 }
